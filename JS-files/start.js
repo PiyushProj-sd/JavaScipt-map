ace91c8251f9739adc0271afdfd76d0ca815b4d2
Piyush <projpiyushsd@gmail.com>
2026-07-25 01:48:50 +0530

    Add logical operators and examples

--- JS-files/start.js
+++ JS-files/start.js
@@ -46,3 +46,14 @@ let c = "5"; //string
 // console.log(a>=b); //false
 // console.log(a<=c); //true
 //here these operators will also not be able to distinguish betweed a string and a number for comparison
+
+//logical operators &&(AND), ||(OR), !(NOT);
+// let cond1 = a>b; //false
+// let cond2 = a<=b; //true
+// let cond3 = a===c; //false
+
+// console.log("cond1 && cond2", cond1&&cond2); //false both should be true
+// console.log("cond1 || cond2", cond1||cond2); //true cause only one cond true is needed
+// console.log("cond1 && cond2 || cond3", cond1&&cond2||cond3); //false
+// here cond1 && cond2 is false so false then cond3 is also false therefore final ans in false
+// console.log("cond1 && cond2", !(cond1 && cond2)); //true cause not(!) will always give opposite answers
