e7732ee7abfa9c439efbc8e9745a7e22bbe48b69
Piyush <projpiyushsd@gmail.com>
2026-07-25 01:25:10 +0530

    pre/post increment and decrement

--- JS-files/start.js
+++ JS-files/start.js
@@ -8,3 +8,15 @@ let b = 6;
 // console.log(a/b);
 // console.log(a%b);
 // console.log(a**b);
+//increment post
+// a++;
+// console.log(a);
+//decrement post
+// a--;
+// console.log("a = ", a);
+
+// console.log("a = ", a++); //here a will give 5;
+// console.log("a = ", a); //here a will give 6;
+
+// console.log("a = ", --a); //here a will first give 4
+
