aecfbc955ff60df6016c6446df68908e5c749438
Piyush <projpiyushsd@gmail.com>
2026-07-25 08:50:46 +0530

    Add strings

---
+++ JS-files/strings.js
@@ -0,0 +1,9 @@
+// let a = "some"+"times";
+// console.log(a);
+// console.log(typeof a);
+
+// // when a sting and number is added together then they're added
+// let b = "hello"+234;
+// console.log(b);
+// console.log(typeof(b)); //string+num = stringl;
+
