86d5b01632744923e090cfa312569abe9883590d
Piyush <projpiyushsd@gmail.com>
2026-07-25 08:50:24 +0530

    modify

--- JS-files/conditionStatement.js
+++ JS-files/conditionStatement.js
@@ -10,10 +10,10 @@
 // }
 
 // check odd or even
-let num = 5;
-num = 562n;
-if(num % 2n == 0){
-  console.log("Number is Even");
-} else {
-  console.log("Number is Odd");
-}
+// let num = 5;
+// num = 562n;
+// if(num % 2n == 0){
+//   console.log("Number is Even");
+// } else {
+//   console.log("Number is Odd");
+// }
