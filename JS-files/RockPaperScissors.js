290041f0a6dc14e3c94211b6e2771478ca80f29b
Piyush <projpiyushsd@gmail.com>
2026-07-29 00:22:21 +0530

    game js nothing inside

---
+++ JS-files/RockPaperScissors.js
@@ -0,0 +1,14 @@
+/* const randomNumber = Math.random();
+
+if(randomNumber >= 0 && randomNumber < 1/3){
+  console.log("Rock");
+
+}else if(randomNumber >=1/3 && randomNumber <2/3){
+  console.log("paper");
+
+}else{
+  console.log("Scissors");
+
+}
+  */
+ 
