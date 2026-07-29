2784220922e91989fa654ec183de35ea2fb8e9d0
Piyush <projpiyushsd@gmail.com>
2026-07-29 18:03:07 +0530

    Add 6b js

--- JS-files/lssn6.js
+++ JS-files/lssn6.js
@@ -1,22 +1,19 @@
-let hour = 17;
-hour = 20;
-hour = 9;
-hour = 24;
-hour = 25;
+let hour = 24;
+const name = 'Piyush';
 
 if(4<=hour && hour<12){
-  console.log("Good Morning!");
-} else if(12<=hour && hour<18){
-  console.log("Good Afternoon!");
+  console.log(`Good Morning! ${name}`);
+} else if(12<hour && hour<18){
+  console.log(`Good Afternoon! ${name}`);
 } else if(18<=hour && hour<24){
-  console.log("Good Night!");
+  console.log(`Good Night! ${name}`);
 } else if(hour !== 12 && hour !==24){
   console.log("invalid time input");
 }
 
 if(hour === 24){
-  console.log("At Mid Night!");
+  console.log(`At Mid Night ${name} Sooja`);
 } else if(hour === 12){
-  console.log("At Noon!");
+  console.log(`At Noon!`);
 }
 
